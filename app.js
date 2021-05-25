4c038a61cbb91542ee7ad467899d96672ffaf3b1
Javier Martinez Ribo <javiermribo@gmail.com>
2021-05-24 21:12:49 -0300

    using bootstrap

--- app.js
+++ app.js
@@ -18,17 +18,27 @@ const selectingHeader = document.querySelector("#header-content");
 const creatingNav = document.createElement("nav");
 selectingHeader.appendChild(creatingNav);
 creatingNav.setAttribute("id", "nav-items");
+creatingNav.setAttribute(
+  "class",
+  "navbar navbar-expand-lg navbar-light bg-light justify-content-around shadow-sm"
+);
+
 
 const selectingNav = document.querySelector("#nav-items");
 const creatingUlNav = document.createElement("ul");
 selectingNav.appendChild(creatingUlNav);
 creatingUlNav.setAttribute("id", "ul-list-items");
+creatingUlNav.setAttribute("class", "nav bg-light p-2 justify-content-around");
 
 const selectingUl = document.querySelector("#ul-list-items");
 const creatingLiItems = document.createElement("li");
 const creatingLiItems2 = document.createElement("li");
 const creatingLiItems3 = document.createElement("li");
+const selectingUl2 = document.querySelector("#ul-list-items");
 creatingLiItems.textContent = "Soy un <li>";
 creatingLiItems2.textContent = "Soy un segundo <li>";
 creatingLiItems3.textContent = "Soy un tercer <li>";
 selectingUl.append(creatingLiItems, creatingLiItems2, creatingLiItems3);
+creatingLiItems.setAttribute("class", "nav-item px-3 text-primary");
+creatingLiItems2.setAttribute("class", "nav-item px-3 text-dark");
+creatingLiItems3.setAttribute("class", "nav-item px-3 text-muted");
